e75f29c443297be51b4cb47a8f49dec1755d5bf9
Shtirlyts <redleg30607@gmail.com>
2025-12-24 21:53:32 +0300

    Добавил файл

---
+++ Code.c
@@ -0,0 +1,26 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <locale.h>
+#include <math.h>
+
+int main() {
+    setlocale(LC_CTYPE, "RUS");
+
+    double x;
+    double sum = 0.0;
+    int n;
+
+    printf("Введите значение x: ");
+    scanf("%lf", &x);
+    printf("Введите максимальную степень: ");
+    scanf("%d", &n);
+
+    for (int i = 0; i <= n; i++) { 
+        double term = sin(pow(x, i));
+        sum += term;
+        printf("sin(x^%d) = %.6f\n", i, term);
+    }
+    printf("Сумма = %.6f\n", sum);
+
+    return 0;
+}
